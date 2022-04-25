90db2d055b5fa546f64658a5c2bde1a9f33892e0
Jappie Klooster <jappieklooster@hotmail.com>
2022-04-25 17:46:48 -0400

    add testcase for the language file

--- test/Main.hs
+++ test/Main.hs
@@ -6,11 +6,12 @@ import JappieLang
 import Text.Trifecta
 import Prettyprinter.Internal
 import Prettyprinter.Render.Terminal.Internal
+import Control.Monad.IO.Class
 
 import Data.List(sort)
 
 main :: IO ()
-main = defaultMain unitTests
+main = defaultMain $ testGroup "tests" [unitTests, langFiles]
 
 toSuccess :: Result a -> a
 toSuccess = \case
@@ -18,16 +19,23 @@ toSuccess = \case
   Failure err -> error $ show err
 
 unitTests :: TestTree
-unitTests = testGroup "Unit tests"
+unitTests = testGroup "unit"
   [
     testCase "parse a lambda" $
-       toSuccess (parseString parseExpr mempty "([x] x)") @?= (Lam (MkName "x") (var "x"))
+       toSuccess (parseString parseExpr mempty "([x] x)") @?= Lam (MkName "x") (var "x")
   , testCase "parse a var" $
-       toSuccess (parseString parseExpr mempty "x") @?= ( (var "x"))
+       toSuccess (parseString parseExpr mempty "x") @?= var "x"
   , testCase "parse an app" $
-       toSuccess (parseString parseExpr mempty "(([x] x) ([y] y))") @?= ( (App (Lam (MkName "x") (var "x")) (Lam (MkName "y") (var "y"))))
+       toSuccess (parseString parseExpr mempty "(([x] x) ([y] y))") @?= App (Lam (MkName "x") (var "x")) (Lam (MkName "y") (var "y"))
   , testCase "parse a comment" $
-       toSuccess (parseString parseExpr mempty "; xxxx") @?= ( (Comment "xxxx"))
+       toSuccess (parseString parseExpr mempty "; xxxx") @?= Comment "xxxx"
   , testCase "parse a comment" $
-       toSuccess (parseString parseExpr mempty "; xxxx ([x] x)") @?= ( (Comment "xxxx ([x] x)"))
+       toSuccess (parseString parseExpr mempty "; xxxx ([x] x)") @?= Comment "xxxx ([x] x)"
+  ]
+
+langFiles :: TestTree
+langFiles = testGroup "Language files"
+  [ testCase "./jappie-lang/lambda.jappie" $ do
+      res <- liftIO $ parseFromFile parseExpr "test/jappie-lang/lambda.jappie"
+      res @?= Just (Comment "comment")
   ]
