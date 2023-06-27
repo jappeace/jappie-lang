1b52636fd9f9d078576e1de2888f94bffc1df0d9
Jappie Klooster <jappieklooster@hotmail.com>
2023-06-26 23:20:32 -0400

    use llvm party

--- flake.nix
+++ flake.nix
@@ -15,27 +15,14 @@
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
       hpkgs = pkgs.haskell.packages.ghc944.override {
-        overrides = hnew: hold: let
-          llvmSrcPure =
-            (builtins.fetchGit {
-                url = "https://github.com/jappeace/llvm-hs";
-                ref = "jappie-patch";
-                rev = "3c9a5ff0c78bce9925eb3c6a2360f5d660f0047f";
-            })
-          ;
-          llvmSrc2 =
-            (builtins.fetchGit {
-                url = "https://github.com/jappeace/llvm-hs";
-                ref = "jappie-patch";
-                rev = "ed5a28b45e9aefd9369d8baab2ff578178ba8744";
-            })
-          ;
-        in {
+        overrides = hnew: hold:
+        {
           jappie-lang  = hnew.callCabal2nix "jappie-lang" ./. { };
-          llvm-hs =
-            (pkgs.haskell.lib.overrideCabal
-              (hnew.callCabal2nix "llvm-hs" "${llvmSrc2}/llvm-hs" {}) { libraryToolDepends = [ pkgs.llvmPackages_12.libllvm ]; });
-          llvm-hs-pure = (hnew.callCabal2nix "llvm-hs-pure" "${llvmSrcPure}/llvm-hs-pure" {});
+          llvm-party = (pkgs.haskell.lib.overrideCabal (hnew.callHackageDirect {
+              pkg = "llvm-party";
+              ver = "12.1.1";
+              sha256 = "sha256-mmorrZiZ3nh8g2BFgykcU6Kwlh/OsDERWrcsPU1zI34=";
+          } {}) { libraryToolDepends = [ pkgs.llvmPackages_12.libllvm ]; });
         };
       };
     in
