e817e416ba3cdffa9909fea6950fa9a53fd278d9
Jappie Klooster <jappieklooster@hotmail.com>
2023-07-09 20:50:40 -0400

    isolate the tests

--- test/Test/Compile.hs
+++ test/Test/Compile.hs
@@ -1,6 +1,8 @@
 
 module Test.Compile(llvmTests) where
 
+import System.IO.Temp
+import System.Directory
 import System.Process.Typed
 import Test.Tasty
 import TestOrphans()
@@ -8,6 +10,8 @@ import Test.Tasty.Golden
 import JappieLang.CodeGen
 import qualified Data.ByteString.Lazy as LBS
 import JappieLang.Cli
+import System.FilePath
+import Data.String
 
 llvmTests :: IO TestTree
 llvmTests = do
@@ -18,8 +22,9 @@ llvmTests = do
 goldenRun :: FilePath -> TestTree
 goldenRun path =
   goldenVsString path (path <> ".expected") $ do
-    compile $ MkCompileOptions {
-      inputFile = (path <>".jappie")
-      , outputFile = "a.out"
-      }
-    readProcessStdout_ "./a.out"
+    withSystemTempDirectory "golden-run-compile" $ \dir -> do
+      compile $ MkCompileOptions {
+        inputFile = (path <>".jappie")
+        , outputFile = dir </> "a.out"
+        }
+      readProcessStdout_ $ fromString $ dir </> "./a.out"
